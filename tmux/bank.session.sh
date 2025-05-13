6ae22bbc452412edc0e3afc50753cf621366f1ce
Niktia Smerdov <76522127+smenikdov@users.noreply.github.com>
2025-05-13 09:12:39 +0500

    Update bank.session.sh

--- tmux/bank.session.sh
+++ tmux/bank.session.sh
@@ -1,23 +1,38 @@
-# Set a custom session root path. Default is `$HOME`.
-# Must be called before `initialize_session`.
-session_root "~/Projects/bank"
+session_root "~/projects/bank"
 
-# Create session with specified name if it does not already exist. If no
-# argument is given, session name will be based on layout file name.
 if initialize_session "vdelo"; then
-  new_window "nvim"
-  run_cmd "nvim ."
+  # new_window "nvim"
+  # run_cmd "nvim .;"
+  #
+  # new_window "vdelo"
+  # run_cmd "nvm use vdelo;"
+  # run_cmd "~/projects/my-scripts/bank-session/run.sh;"
+  #
+  # new_window "master"
+  # run_cmd "nvm use vdelo;"
+  # run_cmd "pm2 start --name backend \"npm run start | tee ~/projects/my-scripts/bank-session/backend.log\";"
+  # run_cmd "pm2 start --name frontend \"npm run dev | tee ~/projects/my-scripts/bank-session/frontend.log\";"
+  #
+  # select_window 1
 
-  new_window "front"
+  new_window "nvim"
+  run_cmd "nvim .;"
+
+  new_window "git"
+  run_cmd "lazygit"
+
+  new_window "master"
   run_cmd "nvm use vdelo"
-  run_cmd "npm run dev"
 
   new_window "back"
-  run_cmd "nvm use vdelo"
+  run_cmd "nvm use 12.22.12"
   run_cmd "npm run start"
+
+  new_window "front"
+  run_cmd "nvm use 12.22.12"
+  run_cmd "npm run dev"
 
   select_window 1
 fi
 
-# Finalize session creation and switch/attach to it.
 finalize_and_go_to_session
